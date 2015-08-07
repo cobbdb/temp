75b6413a16544c312e45a2975ed0bd6acde9479b
Dan <cobbdb@gmail.com>
2015-08-07 11:29:55 -0700

    .

---
+++ bootstrap.js
@@ -0,0 +1,10 @@
+﻿var T = document,
+    V = document.currentScript,
+    U = 'script.js';
+
+if (true) {//!("complete" == T.readyState || "loaded" == T.readyState || V && V.async)) {
+    try {
+        T.write('This is bootstrap content.');
+        //T.write('<script id="' + W + '" src="' + U + '">\x3c/script>')
+    } catch (X) {}
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    var el = document.createElement('h4');
+    el.innerHTML = 'This is final script content';
+    document.body.appendChild(el);
+}());
